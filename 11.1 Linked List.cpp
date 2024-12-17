75b6ea25ab724219fed26dc0d5ca27ca78ef5b01
Aina Mestres <aina.mestres@estudiantat.upc.edu>
2024-12-18 00:34:27 +0100

    Agregar archivos de proyecto.

---
+++ 11.1 Linked List.cpp
@@ -0,0 +1,10 @@
+// 11.1 Linked List.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
